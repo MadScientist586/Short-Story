f3bbe59ff0964b099b0c7fe429b681c7b338aa57
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-13 09:56:16 -0500

    Made a story

---
+++ Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("What is your Name?");
+var name = Console.ReadLine();
+Console.WriteLine("What is your favorite color?");
+var color = Console.ReadLine();
+Console.WriteLine("What is your favorite food?");
+var food = Console.ReadLine();
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Once upon a time there was someone named");
+Console.WriteLine(name);
+Console.WriteLine("Their favorite color was");
+Console.WriteLine(color);
+Console.WriteLine($"They were eating");
+Console.WriteLine(food);
+Console.WriteLine("They were having a good day.");
